277d0cd3755174a0cb3a569471b8bee593c12fe1
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2024-06-15 13:06:36 +0530

    js

---
+++ index.js
@@ -0,0 +1,36 @@
+var x=0;
+var y=0;
+function one(){
+     x=1;    
+}
+function two(){
+     x=2;    
+}
+function three(){
+     x=3;    
+}
+function four(){
+     x=4;    
+}
+function five(){
+     x=5;    
+}
+function six(){
+     x=6;    
+}
+function seven(){
+     x=1;    
+}
+function eight(){
+     x=1;    
+}
+function nine(){
+     x=1;    
+}
+function zero(){
+     x=1;    
+}
+function plus(){
+     y=x
+
+}
